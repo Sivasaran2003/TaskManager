cd1ce0dd71fee1ff53abf3171bcefe2bd4fac1f8
Sivasaran2003 <sivasarancbe.2003@gmail.com>
2025-05-03 23:32:15 +0530

    without controller advice

--- src/main/java/TaskManager/project/project/controllers/TaskController.java
+++ src/main/java/TaskManager/project/project/controllers/TaskController.java
@@ -2,8 +2,13 @@ package TaskManager.project.project.controllers;
 
 import TaskManager.project.project.models.Task;
 import TaskManager.project.project.services.TaskService;
+import exceptions.TaskNotFoundException;
 import jakarta.websocket.server.PathParam;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,38 +20,47 @@ public class TaskController {
     TaskService service;
 
     @GetMapping
-    public List<Task> getAllTasks() {
-        return service.getTasks();
+    public ResponseEntity<List<Task>> getAllTasks() {
+        return ResponseEntity.ok(service.getTasks());
     }
 
     @PostMapping
-    public Task addNewTask(@RequestBody Task task) {
-        return service.addTask(task);
+    public ResponseEntity<Task> addNewTask(@RequestBody Task task) {
+        return new ResponseEntity<>(service.addTask(task), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public Task getByTaskId(@PathVariable int id) {
-        return service.getByTaskId(id);
+    public ResponseEntity<?> getByTaskId(@PathVariable int id) {
+        try {
+            return new ResponseEntity<>(service.getByTaskId(id), HttpStatus.OK);
+        }catch (TaskNotFoundException t) {
+            return new ResponseEntity<>(t.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping
-    public Task updateTask(@RequestBody Task task) {
-        return service.updateTask(task);
+    public ResponseEntity<Task> updateTask(@RequestBody Task task) {
+        return new ResponseEntity<>(service.updateTask(task), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable int id) {
-        service.deleteTask(id);
+    public ResponseEntity<?> deleteTask(@PathVariable int id) {
+        try {
+            service.deleteTask(id);
+            return new ResponseEntity<>("Task deleted", HttpStatus.OK);
+        }catch (TaskNotFoundException t) {
+            return new ResponseEntity<>(t.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/status/{status}")
-    public List<Task> getTasksByStatus(@PathVariable String status) {
-        return service.getTasksByStatus(status);
+    public ResponseEntity<List<Task>> getTasksByStatus(@PathVariable String status) {
+        return new ResponseEntity<>(service.getTasksByStatus(status), HttpStatus.OK);
     }
 
     @GetMapping("/sort")
-    public List<Task> getTasksSorted() {
-        return service.getTasksSorted();
+    public ResponseEntity<List<Task>> getTasksSorted() {
+        return new ResponseEntity<>(service.getTasksSorted(), HttpStatus.OK);
     }
 
 }
